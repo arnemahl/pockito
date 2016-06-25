59dda4a2b6758b3993258d786cc5ad68752235a2
Arne Mæhlum <arne@iterate.no>
2016-06-25 20:55:49 +0200

    Additions, tweaks and minor fixes
    	- Validators:
    		* nonNullObject: removed
    		* object: cannot be null
    		* nullableObject: may be null
    		* arrayOf: added
    		* objectOf: added
    	- Listenable.finalize: add uniValidator to prevent new props
    	- Config: add uniValidator to prevent wrong propNames

--- src/validators/Validators.js
+++ src/validators/Validators.js
@@ -5,19 +5,24 @@ export const string = (value) => typeof value === 'string';
 export const number = (value) => typeof value === 'number';
 export const symbol = (value) => typeof value === 'symbol';
 export const func = (value) => typeof value === 'function';
-export const object = (value) => typeof value === 'object';
+export const object = (value) => typeof value === 'object' && value !== null;
 
 export const undef = (value) => typeof value === 'undefined';
-export const undefOr = (validator) => (value) => undef(value) || validator(value);
 
 export const array = (value) => Array.isArray(value);
 
 export const nonEmptyString = (value) => string(value) && !!string;
-export const nonNullObject = (value) => object(value) && value !== null;
+export const nullableObject = (value) => typeof value === 'object';
 
 export const integer = (value) => Number.isInteger(value);
 export const parsableInteger = (value) => Number.isInteger(Number.parseFloat(value, 10));
 export const parsableFloat = (value) => !Number.isNaN(Number.parseFloat(value, 10));
 
+// Takes arguments
+export const undefOr = (validator) => (value, l, p) => undef(value) || validator(value);
+
 export const oneOf = (possibleValues) => (value) => possibleValues.indexOf(value) !== -1;
 export const oneOfType = (possibleTypes) => (value) => possibleTypes.some(type => type(value));
+
+export const arrayOf = (validator) => (value, l, p) => array(value) && value.every(v => validator(v, l, p));
+export const objectOf = (validator) => (value, l, p) => object(value) && Object.keys(value).map(key => value[key]).every(v => validator(v, l, p));
